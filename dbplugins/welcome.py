5ecb5a902f7bd2422f465d528bf43dba50dc71b5
Germanafk <49345648+Germanafk@users.noreply.github.com>
2019-09-21 17:19:41 +0200

    Update welcome.py

--- dbplugins/welcome.py
+++ dbplugins/welcome.py
@@ -1,7 +1,8 @@
 """Greetings
 Commands:
 .clearwelcome
-.savewelcome <Welcome Message>"""
+.setwelcome <Welcome Message>
+.listwelcome"""
 
 from telethon import events
 from telethon.utils import pack_bot_file_id
@@ -19,26 +20,47 @@ async def _(event):
         user_joined=True,
         user_left=False,
         user_kicked=False,"""
-        if event.user_joined:
+        if event.user_joined or event.user_added:
             if cws.should_clean_welcome:
                 try:
-                    await borg.delete_messages(  # pylint:disable=E0602
+                    await borg.delete_messages(
                         event.chat_id,
                         cws.previous_welcome
                     )
                 except Exception as e:  # pylint:disable=C0103,W0703
                     logger.warn(str(e))  # pylint:disable=E0602
             a_user = await event.get_user()
+            chat = await event.get_chat()
+
+            title = chat.title if chat.title else "this chat"
+            participants = await event.client.get_participants(chat)
+            count = len(participants)
+            msg_o = await event.client.get_messages(
+                entity=Config.PRIVATE_CHANNEL_BOT_API_ID,
+                ids=int(cws.f_mesg_id)
+            )
+            current_saved_welcome_message = msg_o.message
+            file_media = msg_o.media
+            mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
+            first = a_user.first_name
+            last = a_user.last_name
+            if last:
+                fullname = f"{first} {last}"
+            else:
+                fullname = first
+            username = f"@{a_user.username}" if a_user.username else mention
+            userid = a_user.id
             current_saved_welcome_message = cws.custom_welcome_message
             mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
+            
             current_message = await event.reply(
-                current_saved_welcome_message.format(mention=mention),
-                file=cws.media_file_id
+                current_saved_welcome_message.format(mention=mention, title=title, count=count, first=first, last=last, fullname=fullname, username=username, userid=userid),
+                file=cws.file_media
             )
             update_previous_welcome(event.chat_id, current_message.id)
 
 
-@borg.on(admin_cmd("savewelcome"))  # pylint:disable=E0602
+@borg.on(admin_cmd("setwelcome"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
@@ -50,7 +72,7 @@ async def _(event):
     else:
         input_str = event.text.split(None, 1)
         add_welcome_setting(event.chat_id, input_str[1], True, 0)
-        await event.edit("Welcome note saved. ")
+        await event.edit("Welcome Message saved. ")
 
 
 @borg.on(admin_cmd("clearwelcome"))  # pylint:disable=E0602
@@ -63,3 +85,20 @@ async def _(event):
         "Welcome note cleared. " + \
         "The previous welcome message was `{}`.".format(cws.custom_welcome_message)
     )
+
+
+@borg.on(admin_cmd("listwelcome"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    cws = get_current_welcome_settings(event.chat_id)
+    if hasattr(cws, 'custom_welcome_message'):
+        await event.edit(
+            "Welcome note found. " + \
+        "Your welcome message is `{}`.".format(cws.custom_welcome_message)
+    )
+    else:
+        await event.edit(
+            "No Welcome Message found"
+        )
+         
